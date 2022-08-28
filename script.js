cc5ad9eb17e07aaf51f23acaa7736154d176e5f0
Dmitry <112308134+brokenhustlebones@users.noreply.github.com>
2022-08-28 15:19:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+let slidePosition = 1;
+SlideShow(slidePosition);
+
+// forward/Back controls
+function plusSlides(n) {
+    SlideShow(slidePosition += n);
+}
+
+//  images controls
+function currentSlide(n) {
+    SlideShow(slidePosition = n);
+}
+
+function SlideShow(n) {
+    let i;
+    let slides = document.getElementsByClassName("Containers");
+    if (n > slides.length) {slidePosition = 1}
+    if (n < 1) {slidePosition = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slidePosition-1].style.display = "block";
+
+}
